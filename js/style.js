1bea5af4b556a51f532c1c806ea661e8b2e70d1f
saleh-work-pc <9oyle7@gmail.com>
2023-10-29 12:47:59 +0300

    add swiperjs JS for slider

---
+++ js/style.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    centeredSlides: false,
+    spaceBetween: 30,
+    grapCursor: true,
+    loop: true,
+    breakpoints: {
+        480: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+      },
+
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
