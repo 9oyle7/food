14e599acc00d9a94ce0cd7b01e1e85b8de967874
saleh-work-pc <9oyle7@gmail.com>
2023-11-02 13:21:32 +0300

    testing responsive

--- js/style.js
+++ js/style.js
@@ -1,35 +1,30 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
-    centeredSlides: true,
-    spaceBetween: 10,
-    grapCursor: true,
-    loop: true,
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-      },
-      768: {
-        slidesPerView: 5,
-        spaceBetween: 10,
-      },
-      1024: {
-        slidesPerView: 5,
-        spaceBetween: 10,
-      },
-      },
 
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2.5,
+    centeredSlides: false,
+    spaceBetween: 20,
+    loop: true,
     // autoplay: {
     //     delay: 2500,
     //     disableOnInteraction: false,
     // },
-
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+    breakpoints: {
+        640: {
+          slidesPerView: 4,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 5,
+          spaceBetween: 10,
+        },
+        1024: {
+          slidesPerView:8,
+          spaceBetween: 25,
+        },
+        },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
   });
+  
